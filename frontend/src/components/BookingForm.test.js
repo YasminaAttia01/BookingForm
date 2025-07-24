910cc39f4e7786c14c08592611c8c2ae65c6023c
Yasmina Attia <yasmina.attia@pixartprinting.com>
2025-07-24 21:36:12 +0200

    test: add unit tests for BookingForm validation and submission

--- frontend/src/components/BookingForm.test.js
+++ frontend/src/components/BookingForm.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from "./BookingForm";
 import '@testing-library/jest-dom';
